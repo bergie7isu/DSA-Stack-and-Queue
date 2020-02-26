c62141d123ba5718c08cfd45a9a45422ae682d89
Mark Bergstrom <markbergstrom@gmail.com>
2020-02-26 02:12:24 -0600

    initial commit

---
+++ Queue.js
@@ -0,0 +1,126 @@
+class _Node {
+    constructor(value, next) {
+        this.value = value;
+        this.next = next;
+    };
+};
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    };
+    insertFirst(item) {
+        this.head = new _Node(item, this.head);
+    };
+    insertLast(item) {
+        if (this.head === null) {
+            this.insertFirst(item);
+        }
+        else {
+            let tempNode = this.head;
+            while (tempNode.next !== null) {
+                tempNode = tempNode.next;
+            };
+            tempNode.next = new _Node(item, null);
+        };
+    };
+    find(item) {
+        let currNode = this.head;
+        if (!this.head) {
+            return null;
+        };
+        while (currNode.value !== item) {
+            if (currNode.next === null) {
+                return null;
+            }
+            else {
+                currNode = currNode.next;
+            };
+        };
+        return currNode;
+    };
+    remove(item) {
+        if (!this.head) {
+            return null;
+        };
+        if (this.head.value === item) {
+            this.head = this.head.next;
+            return;
+        };
+        let currNode = this.head;
+        let previousNode = this.head;
+        while ((currNode !== null) && (currNode.value !== item)) {
+            previousNode = currNode;
+            currNode = currNode.next;
+        };
+        if (currNode === null) {
+            console.log('Item not found');
+            return;
+        };
+        previousNode.next = currNode.next;
+    };
+    insertBefore(item, beforeItem) {
+        let newNode = new _Node(item, null);
+        if (!this.head) {
+            return null;
+        };
+        if (this.head.value === beforeItem) {
+            newNode.next = this.head;
+            this.head = newNode;
+            return;
+        };
+        let currNode = this.head;
+        let previousNode = this.head;
+        while ((currNode !== null) && (currNode.value !== beforeItem)) {
+            previousNode = currNode;
+            currNode = currNode.next;
+        };
+        if (currNode === null) {
+            console.log(`Cannot find ${beforeItem}`);
+        };
+        newNode.next = currNode;
+        previousNode.next = newNode;
+    };
+    insertAfter(item, afterItem) {
+        let newNode = new _Node(item, null);
+        if (!this.head) {
+            return null;
+        };
+        let currNode = this.head;
+        while ((currNode !== null) && (currNode.value !== afterItem)) {
+            currNode = currNode.next;
+        };
+        if (currNode === null) {
+            console.log(`Cannot find ${afterItem}`);
+        };
+        newNode.next = currNode.next;
+        currNode.next = newNode;
+    };
+    insertAt(item, position) {
+        let newNode = new _Node(item, null);
+        if (position < 0) {
+          console.log('position is out of bounds');
+          return;
+        };
+        if (position === 0) {
+            newNode.next = this.head;
+            this.head = newNode;
+            return;
+        };
+        let currNode = this.head;
+        let previousNode = this.head
+        let index = 0;
+        while ((currNode !== null) && (index !== position)) {
+            previousNode = currNode;
+            currNode = currNode.next;
+            index++;
+        };
+        if (currNode === null) {
+            console.log('position is out of bounds');
+        };
+        newNode.next = currNode;
+        previousNode.next = newNode;
+    };
+};
+
+module.exports = LinkedList;
---
+++ drills.js
@@ -0,0 +1,134 @@
+//1. Create a stack class
+const Stack = require('./Stack');
+const starTrek = new Stack();
+starTrek.push('Kirk');
+starTrek.push('Spock');
+starTrek.push('McCoy');
+starTrek.push('Scotty');
+
+//2. Useful methods for a stack
+const peek = function(stack) {
+  if (stack.top === null) {
+    return 'Empty stack!';
+  };
+  return stack.top.data;
+};
+console.log(peek(starTrek));
+
+const isEmpty = function(stack) {
+  if (stack.top === null) {
+    return true;
+  }
+  return false;
+};
+console.log(isEmpty(starTrek));
+const starTrek2 = new Stack();
+console.log(isEmpty(starTrek2));
+
+const display = function(stack) {
+  if (stack.top === null) {
+    return 'Empty stack!';
+  };
+  let stackArray = [];
+  let currNode = stack.top;
+  while (currNode !== null) {
+    stackArray.push(currNode.data);
+    currNode = currNode.next;
+  };
+  return stackArray;
+};
+console.log(display(starTrek));
+
+//3. Check for palindromes using a stack
+function is_palindrome(s) {
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  const palindromeStack = new Stack();
+  for (let character of s) {
+    palindromeStack.push(character);
+  };
+  let palindromeString = '';
+  while (!isEmpty(palindromeStack)) {
+    palindromeString += palindromeStack.pop();
+  }
+  return palindromeString === s;
+}
+// True, true, true, false
+console.log(is_palindrome("dad"));
+console.log(is_palindrome("A man, a plan, a canal: Panama"));
+console.log(is_palindrome("1001"));
+console.log(is_palindrome("Tauhida"));
+
+//4. Matching parentheses in an expression
+const parser = function(string) {
+  const parserStack = new Stack();
+  let singleQuoteActive = false;
+  let doubleQuoteActive = false;
+  const pairs = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  };
+  for (let character in string) {
+    if ((string[character] === "'") && (doubleQuoteActive === false)) {
+      singleQuoteActive = !singleQuoteActive;
+    };
+    if ((string[character] === '"') && (singleQuoteActive === false)) {
+      doubleQuoteActive = !doubleQuoteActive;
+    };
+    if (((string[character] === '(') || (string[character] === '[') || (string[character] === '{')) && (singleQuoteActive === false) && (doubleQuoteActive === false)) {
+      parserStack.push({
+        character: string[character],
+        location: character
+      });
+    };
+    if (((string[character] === ')') || (string[character] === ']') || (string[character] === '}')) && (singleQuoteActive === false) && (doubleQuoteActive === false)) {
+      if (isEmpty(parserStack)) {
+        const expectedChar = Object.keys(pairs).find(key => pairs[key] === string[character]);
+        return `Missing opening '${expectedChar}' before the closing '${string[character]}' at location ${character}!`;
+      };
+      let previousStackNode = parserStack.pop();
+      if (string[character] !== pairs[previousStackNode.character]) {
+        return `Expecting a '${pairs[previousStackNode.character]}' but found a '${string[character]}' at location ${character}!`;
+      };
+    };
+  };
+  if (isEmpty(parserStack)) {
+    return 'All is well with your brackets and nesting. Well done!';
+  } else {
+    let currNode = parserStack.top;
+    let missingCloser;
+    while (currNode !== null) {
+      missingCloser = parserStack.pop();
+      console.log(`Missing closing '${pairs[missingCloser.character]}' after the opening '${missingCloser.character}' at location ${missingCloser.location}!`);
+      currNode = currNode.next;
+    };
+    return false;
+  };
+  return true;
+};
+console.log(parser("([{M'a({[r'k}])"));
+
+//5. Sort stack
+const sortStack = function(stack) {
+  const sortedStack = new Stack();
+  if (stack.top === null) {
+    return 'Empty stack!';
+  };
+  let temp;
+  while (stack.top) {
+    temp = stack.pop();
+    while (sortedStack.top && sortedStack.top.data < temp) {
+      stack.push(sortedStack.pop());
+    };
+    sortedStack.push(temp);
+  };
+  return display(sortedStack);
+};
+const stackToSort = new Stack();
+stackToSort.push(7);
+stackToSort.push(3);
+stackToSort.push(9);
+stackToSort.push(1);
+stackToSort.push(4);
+console.log(display(stackToSort));
+console.log(sortStack(stackToSort));
